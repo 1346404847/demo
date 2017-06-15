5a8ba6c5f5b02c16f2916f65337ef365f421b7fe
gaoguanghui <gaoguanghui@playcrab.com>
2017-06-15 16:54:57 +0800

    初始化项目

---
+++ common/config/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@demo', dirname(dirname(__DIR__)) . '/demo');
+Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
